93624f003a6293cc350596b1f4b01ee616e3a6c9
Sahat Yalkabov <sakhat@gmail.com>
2013-10-29 22:06:33 -0400

    Moved new order javascript handlers to main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,6 @@
-// Items Page
+/**
+ * Items page
+ */
 $('.btn-danger').click(function(eventObject) {
   var id = $(eventObject.target).data('id');
   $.ajax({
@@ -7,5 +9,31 @@ $('.btn-danger').click(function(eventObject) {
     success: function() {
       $(eventObject.target).closest('tr').remove();
     }
+  });
+});
+
+/**
+ * New Order page
+ */
+$('.btn-success').click(function(eventObject) {
+  var items = [];
+  var orderType = $(eventObject.target).data('order-type')
+  var $quantityFields = $('.quantity');
+
+  $.each($quantityFields, function(index, field) {
+    items.push({
+      name: $(field).data('name'),
+      quantity: $(field).val()
+    });
+  });
+
+  var postData = {
+    orderType: orderType,
+    items: items,
+    location: $('button.active').text()
+  };
+
+  $.post('/orders', postData, function() {
+    location.href = '/';
   });
 });
